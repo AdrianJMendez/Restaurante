975b9ce64da33345f013405d37121a5dd13c548d
Orkar504+ <jose7211963@gmail.com>
2024-08-12 03:33:40 -0600

    create inventarioRepositorio

---
+++ Restaurante/src/main/java/unah/lenguajes/Restaurante/repositorios/inventarioRepositorio.java
@@ -0,0 +1,11 @@
+package unah.lenguajes.Restaurante.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import unah.lenguajes.Restaurante.modelos.inventarioModelo;
+
+@Repository
+public interface inventarioRepositorio extends JpaRepository<inventarioModelo,Integer>{
+
+}
