fc6bb50ae2c41f3a5a71b8f025783ec51b08191e
Orkar504+ <jose7211963@gmail.com>
2024-08-12 02:59:18 -0600

    add clienteServicio cliente repositorio,

---
+++ Restaurante/src/main/java/unah/lenguajes/Restaurante/modelos/clienteModelo.java
@@ -0,0 +1,25 @@
+package unah.lenguajes.Restaurante.modelos;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table (name = "cliente")
+@Data
+public class clienteModelo {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "clienteid")
+    private int clienteid;
+
+    private String nombre;
+    private String apellido;
+    private String identificacion;
+
+}
